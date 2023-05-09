83fbeccd201fcd6f24569e93b4ed378471d6d972
plasticine <975036719@qq.com>
2023-05-10 00:17:46 +0800

    feat(projects): 使用 soybean admin 模板

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react'
-import { Admin } from 'react-admin'
-
-const App: FC = () => {
-  return <Admin></Admin>
-}
-
-export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,59 @@
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite';
+import { createViteProxy, getRootPath, getSrcPath, setupVitePlugins, viteDefine } from './build';
+import { getServiceEnvConfig } from './.env-config';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve('./src'),
+export default defineConfig(configEnv => {
+  const viteEnv = loadEnv(configEnv.mode, process.cwd()) as unknown as ImportMetaEnv;
+
+  const rootPath = getRootPath();
+  const srcPath = getSrcPath();
+
+  const isOpenProxy = viteEnv.VITE_HTTP_PROXY === 'Y';
+  const envConfig = getServiceEnvConfig(viteEnv);
+
+  return {
+    base: viteEnv.VITE_BASE_URL,
+    resolve: {
+      alias: {
+        '~': rootPath,
+        '@': srcPath,
+        'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js'
+      }
     },
-  },
-})
+    define: viteDefine,
+    plugins: setupVitePlugins(viteEnv),
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "./src/styles/scss/global.scss" as *;`
+        }
+      }
+    },
+    server: {
+      host: '0.0.0.0',
+      port: 3200,
+      open: true,
+      proxy: createViteProxy(isOpenProxy, envConfig)
+    },
+    optimizeDeps: {
+      include: [
+        '@antv/data-set',
+        '@antv/g2',
+        '@better-scroll/core',
+        'echarts',
+        'swiper',
+        'swiper/vue',
+        'vditor',
+        'wangeditor',
+        'xgplayer'
+      ]
+    },
+    build: {
+      reportCompressedSize: false,
+      sourcemap: false,
+      commonjsOptions: {
+        ignoreTryCatch: false
+      }
+    }
+  };
+});
