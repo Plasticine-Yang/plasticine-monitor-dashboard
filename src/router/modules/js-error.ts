949905ec7fb496955097cfea7472c0e876060e96
plasticine <975036719@qq.com>
2023-05-14 14:14:03 +0800

    feat: 新增运行时异常监控菜单入口

---
+++ src/router/modules/js-error.ts
@@ -0,0 +1,13 @@
+const jsError: AuthRoute.Route = {
+  name: 'js-error',
+  path: '/js-error',
+  component: 'self',
+  meta: {
+    title: '运行时异常监控',
+    icon: 'mdi:monitor-dashboard',
+    singleLayout: 'basic',
+    order: 1
+  }
+};
+
+export default jsError;
---
+++ src/views/js-error/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>JavaScript 运行时异常监控</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
